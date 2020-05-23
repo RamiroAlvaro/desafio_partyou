4b25f0e8012fbea8ff98455ec1fdebea28d47eb4
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-05-23 13:01:52 -0300

    Created home page
    close #19

--- partyou/core/tests/test_home.py
+++ partyou/core/tests/test_home.py
@@ -1,2 +1,22 @@
-def test_tosco():
-    assert 1 == 1
+import pytest
+from django.urls import reverse
+
+from partyou.django_assertions import assert_contains, assert_equal
+
+
+@pytest.fixture
+def resp(client, db):
+    resp = client.get(reverse('home'))
+    return resp
+
+
+def test_status_code(resp):
+    assert_equal(resp.status_code, 200)
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Partyou - Desafio</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'<a class="navbar-brand" href="{reverse("home")}">Partyou</a>')
