68da9111df78c443bd5ffe1498b075f67ba469ea
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-05-23 13:01:52 -0300

    Created home page
    close #19

--- partyou/core/views.py
+++ partyou/core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render # noqa
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'core/home.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title': 'Partyou o Desafio'})
