a7b5c4a0467415e49f7523307af0c7bdcc0b672b
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-05-27 22:08:15 -0300

    Test page email update

--- partyou/conftest.py
+++ partyou/conftest.py
@@ -11,3 +11,15 @@ def user_admin(db, django_user_model):
 def client_user_admin(client, user_admin):
     client.force_login(user_admin)
     return client
+
+
+@pytest.fixture
+def user_logged(db, django_user_model):
+    user = baker.make(django_user_model, first_name='Ramiro')
+    return user
+
+
+@pytest.fixture
+def client_with_logged_user(user_logged, client):
+    client.force_login(user_logged)
+    return client
---
+++ partyou/registration/tests/test_page_email.py
@@ -0,0 +1,34 @@
+import pytest
+from django.urls import reverse
+
+from partyou.django_assertions import assert_contains, assert_equal, assert_template_used, assert_not_contains
+
+
+@pytest.fixture
+def resp(client_with_logged_user, db):
+    resp = client_with_logged_user.get(reverse('profile_email'))
+    return resp
+
+
+def test_status_code(resp):
+    assert_equal(resp.status_code, 200)
+
+
+def test_profile_email_template(resp):
+    assert_template_used(resp, 'registration/profile_email_form.html')
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Email</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'<a class="navbar-brand" href="{reverse("home")}">Partyou</a>')
+
+
+def test_login_link(resp):
+    assert_not_contains(resp, f'<a class="nav-link" href="{reverse("login")}">Acessar</a>')
+
+
+def test_logout_link(resp):
+    assert_contains(resp, f'<a class="nav-link" href="{reverse("logout")}">Sair</a>')
