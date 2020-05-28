d38f3c8c1f92e02fdebcf621c9d8076fa880f72c
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-05-28 20:04:29 -0300

    test page profile

--- partyou/registration/forms.py
+++ partyou/registration/forms.py
@@ -24,8 +24,8 @@ class ProfileForm(forms.ModelForm):
         fields = ['avatar', 'bio', 'link']
         widgets = {
             'avatar': forms.ClearableFileInput(attrs={'class': 'form-control-file mt-3'}),
-            'bio': forms.Textarea(attrs={'class': 'form-control mt-3', 'rows': 3, 'placeholder': 'Biografía'}),
-            'link': forms.URLInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Enlace'}),
+            'bio': forms.Textarea(attrs={'class': 'form-control mt-3', 'rows': 3, 'placeholder': 'Biografia'}),
+            'link': forms.URLInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Web site'}),
         }
 
 
